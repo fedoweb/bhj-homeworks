0b4e65c8554e29aaceb459f9bf8daa11f15ba213
fedoweb <fedo.web@mail.ru>
2024-12-18 22:29:24 +0300

    change homeWork TODO

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,38 +1,26 @@
 const tasksList = document.getElementById('tasks__list');
-const taskInput = document.getElementById('task__input');
 const addButton = document.getElementById('tasks__add');
-
-let text = '';
-
-function createToDo(text) {
-    tasksList.insertAdjacentHTML('afterbegin', `
-        <div class="task">
-            <div class="task__title">
-            ${text}
-            </div>
-            <a href="#" class="task__remove">&times;</a>
-        </div>
-    `);
-
-    const links = tasksList.querySelectorAll('.task__remove');
-
-    links.forEach(link => {
-        link.addEventListener('click', () => {
-            const deleteElement = link.closest('.task');
-            deleteElement.remove();
-        })
-    });
-};
 
 addButton.addEventListener('click', (e) => {
     e.preventDefault();
-
-    text = taskInput.value.trim();
+    const taskInput = document.getElementById('task__input');
+    let text = taskInput.value.trim();
  
     if (text.length > 0) {
-        createToDo(text);
-        taskInput.value = '';
-    } else {
-        taskInput.value = '';
-    }
+        tasksList.insertAdjacentHTML('afterbegin', `
+            <div class="task">
+                <div class="task__title">
+                ${text}
+                </div>
+                <a href="#" class="task__remove">&times;</a>
+            </div>
+        `);
+    } 
+
+    taskInput.value = '';
+});
+
+tasksList.addEventListener('click', (e) => {
+    const deleteElement = e.target.closest('.task');
+    deleteElement.remove();
 });
