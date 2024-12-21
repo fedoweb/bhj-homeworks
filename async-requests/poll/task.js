185706ed0ffa4aeb423c21dc4bf331f145c74b28
fedoweb <fedo.web@mail.ru>
2024-12-21 23:23:36 +0300

    add homeWork POLL

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -1,19 +1,18 @@
 
-let xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://students.netoservices.ru/nestjs-backend/poll');
-xhr.responseType = 'json';
-xhr.send();
+const xhrGet = new XMLHttpRequest();
+xhrGet.open('GET', 'https://students.netoservices.ru/nestjs-backend/poll');
+xhrGet.responseType = 'json';
+xhrGet.send();
 
 const question = document.getElementById('poll__title');
 const answers = document.getElementById('poll__answers');
+const xhrPost = new XMLHttpRequest;
 
-xhr.onload = function() {
-    console.log(xhr.response.data);
+xhrGet.onload = function() {
     
-    question.insertAdjacentHTML('afterbegin', `${xhr.response.data.title}`);
-
+    question.insertAdjacentHTML('afterbegin', `${xhrGet.response.data.title}`);
     
-    const responseAnswers = xhr.response.data.answers;
+    const responseAnswers = xhrGet.response.data.answers;
 
     responseAnswers.forEach(answer => {
         answers.insertAdjacentHTML('beforeend', `
@@ -24,8 +23,41 @@ xhr.onload = function() {
 
         const answerButton = answers.lastElementChild;
         answerButton.addEventListener('click', (e) => {
-            console.log(e.target);
+            let answerTarget = e.target.textContent;
             alert('Спасибо, ваш голос засчитан!');
+            
+            const answerIndex = responseAnswers.indexOf(answerTarget.trim());
+            const answerPost = `vote=${xhrGet.response.id}&answer=${answerIndex}`;
+                        
+            xhrPost.open('POST', 'https://students.netoservices.ru/nestjs-backend/poll');
+            xhrPost.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+            xhrPost.responseType = 'json';
+            xhrPost.send(answerPost);
+
+            
         });
-    });    
+    }); 
 };
+
+
+xhrPost.onload = function() {  
+    const stats = xhrPost.response.stat;
+
+    let totalQuantity = 0;
+
+    stats.forEach(stat => {
+        return totalQuantity += stat.votes;
+    });
+
+    stats.forEach(stat => {
+        answers.innerHTML = '';
+        question.insertAdjacentHTML('beforeend', `
+            <div>
+            ${stat.answer}: ${(stat.votes / totalQuantity * 100).toFixed(2)} %
+            </div>
+        `);
+    });
+};
+
+
+
